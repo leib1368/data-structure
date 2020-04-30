1cbe0ae3e9b525235128f3a8eedde3e3fb9b890b
leib1368 <leib1368@gmail.com>
2020-04-30 12:55:17 +0800

    习题更新

---
+++ src/leib/leetcode0204/TreeNode.java
@@ -0,0 +1,14 @@
+package leib.leetcode0204;
+
+/**
+ * @Author:Leib
+ * @Date 2020/4/30 11:30
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+     val = x;
+    }
+}
---
+++ src/leib/leetcode0205/Test0501.java
@@ -0,0 +1,12 @@
+package leib.leetcode0205;
+
+/**
+ * @Author:Leib
+ * @Date 2020/4/30 12:52
+ */
+public class Test0501 {
+
+    public int t(){
+        return 0;
+    }
+}
