c5cc4c8e57aa2a1ee04d33f83a2d52d9e2ab90bd
leib1368 <leib1368@gmail.com>
2020-05-02 16:14:00 +0800

    无重复字符的最长子串  假期快乐~

--- src/leib/leetcode0204/TestBreak.java
+++ src/leib/leetcode0204/TestBreak.java
@@ -3,27 +3,21 @@ package leib.leetcode0204;
 public class TestBreak {
 
     public static void main(String[] args) {
-
         testBreak();
         testContinue();
         testReturn();
-
         System.out.println("--主函数------------");
-
     }
-
     /**
      * break测试方法
      */
     private static void testBreak() {
         for (int i = 0; i < 5; i++) {
-
             if (i == 3) {
                 break;
             }
             System.out.println("--break------------" + i);
         }
-
         System.out.println("--break-------for循环外-----");
 
     }
@@ -33,13 +27,11 @@ public class TestBreak {
      */
     private static void testContinue() {
         for (int i = 0; i < 5; i++) {
-
             if (i == 3) {
                 continue;
             }
             System.out.println("--continue------------" + i);
         }
-
         System.out.println("--continue-------for循环外-----");
 
     }
@@ -49,13 +41,11 @@ public class TestBreak {
      */
     private static void testReturn() {
         for (int i = 0; i < 5; i++) {
-
             if (i == 3) {
                 return;
             }
             System.out.println("--return------------" + i);
         }
-
         System.out.println("--return-------for循环外-----");
 
     }
