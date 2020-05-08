14039280ce505fc8b7e607bfe0e6e08794e2869a
leib1368 <leib1368@gmail.com>
2020-05-08 21:02:31 +0800

    0508 一些面试习题

---
+++ src/leib/leetcode0205/Parent.java
@@ -0,0 +1,14 @@
+package leib.leetcode0205;
+
+/**
+ * @Author:Leib
+ * @Date 2020/5/8 19:33
+ */
+public class Parent {
+    public static void  staticMethod(){
+        System.out.println("Parent staticMethod run");
+    }
+    public void method(){
+        System.out.println("Parent method run");
+    }
+}
---
+++ src/leib/leetcode0205/Test.java
@@ -0,0 +1,39 @@
+package leib.leetcode0205;
+
+/**
+ * @Author:Leib
+ * @Date 2020/5/8 19:34
+ */
+public class Test {
+    public static void main(String[] args) {
+/*        Parent p = new Parent();
+        p.staticMethod();
+
+        Parent child=new Child();
+        child.staticMethod();//输出：Parent staticMethod run
+
+
+        Child c=new Child();
+        c.staticMethod();
+        child.method();//这样才存在多态*/
+
+        //Integer的缓存范围(-128~127)
+        Integer i1 = 6;
+        Integer i2 = 6;
+        System.out.println((i1==i2));//true
+
+
+        Integer i3 = new Integer(6);
+        Integer i4 = new Integer(6);
+
+        System.out.println((i3==i4));//false
+        System.out.println((6==i3));
+        System.out.println((i3==6));
+        System.out.println((128==i3));
+        System.out.println((i4==128));
+
+        System.out.println((i3==i4)+" "+i3.hashCode()+" "+i4.hashCode());
+
+
+    }
+}
