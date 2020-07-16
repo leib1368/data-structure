ca1543c70f6ff822569d5367834dbeb763f3208c
leib1368 <leib1368@gmail.com>
2020-07-16 20:59:37 +0800

    习题更新

---
+++ src/main/java/leib/demo/leecode/leetcode2007/T0708.java
@@ -0,0 +1,25 @@
+package leib.demo.leecode.leetcode2007;
+
+import java.util.Hashtable;
+
+/**
+ * @Author:Leib
+ * @Date 2020/7/8 10:09
+ */
+public class T0708 {
+    public static void main(String[] args) {
+        int x = args.length;
+        System.out.println(x);
+
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+
+        Hashtable b = new Hashtable();
+        b.put(1,1);
+
+    }
+}
---
+++ src/main/java/leib/demo/leecode/leetcode2007/T0709.java
@@ -0,0 +1,51 @@
+package leib.demo.leecode.leetcode2007;
+
+import java.util.Arrays;
+
+/**
+ * @Author:Leib
+ * @Date 2020/7/9 20:56
+ */
+public class T0709 {
+    public static void main(String[] args) {
+        titleToNumber("AAA");
+        System.out.println(countPrimes1(15));
+    }
+    public static int titleToNumber(String s) {
+        int ans = 0 ;
+        for (int i = 0; i < s.length(); i++) {
+            int num = s.charAt(i) - 'A' + 1;
+            ans = ans * 26 + num;
+        }
+        return ans ;
+    }
+//    public static int countPrimes(int n) {
+//        int count = 0;
+//        for (int i = 2; i < n; i++)
+//            if (isPrime(i)) count++;
+//        return count;
+//    }
+//    // 判断整数 n 是否是素数
+//    public static boolean isPrime(int n) {
+//        for (int i = 2; i < n; i++)
+//            if (n % i == 0)
+//                // 有其他整除因子
+//                return false;
+//        return true;
+//    }
+
+    public static int countPrimes1(int n) {
+        boolean[] isPrim = new boolean[n];
+        Arrays.fill(isPrim, true);
+        for (int i = 2; i * i < n; i++)
+            if (isPrim[i])
+                for (int j = i * i; j < n; j += i)
+                    isPrim[j] = false;
+
+        int count = 0;
+        for (int i = 2; i < n; i++)
+            if (isPrim[i]) count++;
+
+        return count;
+    }
+}
