41d9f80324989dd3e42348329fe1fae0f987792f
leib1368 <leib1368@gmail.com>
2020-09-11 20:56:23 +0800

    习题更新

---
+++ src/main/java/leib/demo/leecode/leetcode2009/T0911.java
@@ -0,0 +1,18 @@
+package leib.demo.leecode.leetcode2009;
+
+import org.junit.Test;
+
+/**
+ * @Author:Leib
+ * 留着明天写2333
+ * @Date 2020/9/11 20:55
+ */
+public class T0911 {
+    public static void main(String[] args) {
+
+    }
+    @Test
+    public void t(){
+
+    }
+}
--- src/main/java/leib/demo/leecode/leetcode2009/TreeNode.java
+++ src/main/java/leib/demo/leecode/leetcode2009/TreeNode.java
@@ -12,4 +12,28 @@ public class TreeNode {
     TreeNode(int x) {
         val = x;
     }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
 }
