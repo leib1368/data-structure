e0becbbffd077aef22a3ebf6892ac205e5c24629
leib1368 <leib1368@gmail.com>
2020-09-15 22:16:47 +0800

    习题更新

---
+++ src/main/java/leib/demo/leecode/leetcode2009/T0915.java
@@ -0,0 +1,19 @@
+package leib.demo.leecode.leetcode2009;
+
+/**
+ * @Author:Leib
+ * @Date 2020/9/15 22:05
+ */
+public class T0915 {
+    public static void main(String[] args) {
+
+        Person p = new Person("a");
+        System.out.println(p.name);
+    }
+}
+class Person {
+    String name ;
+    Person(String name ){
+
+    }
+}
