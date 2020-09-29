5861fffec3b56a964086c48f96a7271834bd61da
leib1368 <leib1368@gmail.com>
2020-09-29 13:13:36 +0800

    习题更新

--- src/main/java/leib/demo/leecode/leetcode2009/T0928.java
+++ src/main/java/leib/demo/leecode/leetcode2009/T0928.java
@@ -17,7 +17,45 @@ public class T0928 {
         T0928 t0928 = new T0928();
         int [] list = {10,-3,0,5,9};
         TreeNode treeNode = t0911.sortedArrayToBST(list);
-        t0928.levelOrder(treeNode);
+        Node n1 = new Node (1);
+        Node n2 = new Node (2);
+        Node n3 = new Node (3);
+        Node n4 = new Node (4);
+        n1.left = n2;
+        n1.right = n3;
+        n2.left = n4;
+
+        t0928.connect(n1);
+    }
+    public Node connect(Node root) {
+        if (root == null)
+            return root;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            //每一层的数量
+            int levelCount = queue.size();
+            //前一个节点
+            Node pre = null;
+            for (int i = 0; i < levelCount; i++) {
+                //出队
+                Node node = queue.poll();
+                //如果pre为空就表示node节点是这一行的第一个，
+                //没有前一个节点指向他，否则就让前一个节点指向他
+                if (pre != null) {
+                    pre.next = node;
+                }
+                //然后再让当前节点成为前一个节点
+                pre = node;
+                //左右子节点如果不为空就入队
+                if (node.left != null)
+                    queue.add(node.left);
+                if (node.right != null)
+                    queue.add(node.right);
+            }
+        }
+        return root;
+
     }
 
     public List<List<Integer>> levelOrder(TreeNode root) {
@@ -46,6 +84,25 @@ public class T0928 {
         return ans;
     }
 }
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
 enum season {
     red,blue;
 }
---
+++ src/main/java/leib/demo/leecode/leetcode2009/T0930.java
@@ -0,0 +1,8 @@
+package leib.demo.leecode.leetcode2009;
+
+/**
+ * @Author:Leib
+ * @Date 2020/9/28 20:22
+ */
+public class T0930 {
+}
