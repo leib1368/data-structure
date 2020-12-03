50d59ecad27d64a5221a33cde2e408ca081ea0ce
leib1368 <leib1368@gmail.com>
2020-12-03 10:11:38 +0800

    习题更新

---
+++ src/main/java/leib/demo/leecode/leetcode2011/Person.java
@@ -0,0 +1,11 @@
+package leib.demo.leecode.leetcode2011;
+
+/**
+ * @Author:Leib
+ * @Date 2020/11/11 15:11
+ */
+public class Person {
+    public void Travel (TravelMode travelMode){
+        //...
+    }
+}
---
+++ src/main/java/leib/demo/leecode/leetcode2012/T1203.java
@@ -0,0 +1,11 @@
+package leib.demo.leecode.leetcode2012;
+
+/**
+ * @Author:Leib
+ * @Date 2020/12/3 10:10
+ */
+public class T1203 {
+    public static void main(String[] args) {
+
+    }
+}
