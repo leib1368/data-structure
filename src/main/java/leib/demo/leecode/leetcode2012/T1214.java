75da20633ae8784fee9b333a8b8a3ccfffc838b1
leib1368 <leib1368@gmail.com>
2020-12-14 21:35:26 +0800

    nbcb 01

--- src/main/java/leib/demo/leecode/leetcode2012/T1214.java
+++ src/main/java/leib/demo/leecode/leetcode2012/T1214.java
@@ -6,6 +6,8 @@ package leib.demo.leecode.leetcode2012;
  */
 public class T1214 {
     public static void main(String[] args) {
-        System.out.println("---");
+        System.out.println("this is for master branch ");
+        System.out.println("--------------------------");
+        System.out.println("this is for nbcb-01 ");
     }
 }
