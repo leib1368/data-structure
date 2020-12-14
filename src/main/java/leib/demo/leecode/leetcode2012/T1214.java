0df32281746d0fa9c400cddf952b4b267a28bc74
leib1368 <leib1368@gmail.com>
2020-12-14 22:13:25 +0800

    nbcb-001

--- src/main/java/leib/demo/leecode/leetcode2012/T1214.java
+++ src/main/java/leib/demo/leecode/leetcode2012/T1214.java
@@ -7,6 +7,6 @@ package leib.demo.leecode.leetcode2012;
 public class T1214 {
     public static void main(String[] args) {
         System.out.println("21点56分");
-        System.out.println("this is nbcb");
+        System.out.println("this is future");
     }
 }
